f1885dacdf0a02973757a7d7577588527b57f441
Vladimir Frolov <volodymyr.frolov@gmail.com>
2013-02-07 00:35:42 +0200

    First source file added

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  simple-space
+//
+//  Created by Vladimir Frolov on 07.02.13.
+//  Copyright (c) 2013 Vladimir Frolov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, Simple Space!\n";
+    return 0;
+}
+
